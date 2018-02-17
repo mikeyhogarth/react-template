19804a11a7b5d2245ae6582737905ca17cc499e9
Ben Mulhern <ben@benmulhern.co.uk>
2018-02-17 15:44:47 +0000

    Code! That doesn't quite work...

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+const HelloWorld = require('./components/HelloWorld.js')
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('content')
+)
